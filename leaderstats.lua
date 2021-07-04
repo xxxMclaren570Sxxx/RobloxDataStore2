b7a5c3a4f2e7187ba807553a5de611167b40a76c
xxxMclaren570Sxxx <58083410+xxxMclaren570Sxxx@users.noreply.github.com>
2021-07-04 14:46:40 -0400

    Create leaderstats.lua

---
+++ leaderstats.lua
@@ -0,0 +1,25 @@
+local DataStore2 = require(1936396537)
+
+local defaulValue = 0
+
+game.Players.PlayerAdded:Connect(function(plr)
+  print("Data Loaded")
+  local moneyDataStore = DataStore2("moeny", plr)
+  
+  local leaderstats = Instance.new("Folder")
+  leaderstats.Name = "leaderstat"
+  leaderstats.Parent = plr
+  
+  local money = Instance.new("IntValue")
+  money.name = "Money"
+  money.Parent = plr
+  
+  local function moneyUpdate(updatedValue
+    money.Value = moneyDataStore:Get(updatedValue)
+    
+  end
+  
+  moneyUpdate(defaultValue)
+  moneyDataStore:onUpdate(moneyUpdate)
+
+end)
